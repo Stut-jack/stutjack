9d414d6cdbceef96ca288ef848184ad20d7aad3b
Stut-jack <44525482+Stut-jack@users.noreply.github.com>
2018-11-10 08:45:05 +0700

    Create jack.sh

---
+++ jack.sh
@@ -0,0 +1,102 @@
+#!/system/xbin/bash
+#MauNgapainGblk?
+#DiRecodeGakBakalJadiinLuMastah
+#DiGantiAuthorGakBakalJadiinLuMaster
+#MikirMakeOtakGblk
+#KontolKaloMauReedit/Recode 
+#Pm Gua Bangsat
+clear
+cd module
+python2 iz.py
+blue='\033[34;1m'
+green='\033[32;1m'  
+purple='\033[35;1m'
+cyan='\033[36;1m'
+red='\033[31;1m'
+white='\033[37;1m'                                           
+yellow='\033[33;1m'
+sleep 1
+echo $red
+cat asw.txt
+echo ""
+echo "\033[34;1m[$]\033[36;1m============================\033[34;1m[$]"
+echo "\033[32;1mSilahkan Pilih Tools Yang mau Kamu pake:)?:"
+echo "\033[36;1m"
+echo "[===============================================[>"
+echo $green "1.> Spam tlfn Tokopedia"
+echo "[===============================================]>"
+echo $purple "2.> Spam Sms Telkomnyet"
+echo "[===============================================]>"
+echo $cyan "3.> Tools autolike facebook"
+echo "[===============================================]>"
+echo $red "4.> Tools hack facebook brute"
+echo "[===============================================]>"
+echo $white "5.> Install InstaHack buat hack Instagram"
+echo "[===============================================]>"
+echo $yellow "0.> keluar"
+echo "[===============================================]>"
+echo "\033[32;1m"
+read -p "root@T00Ls-Stut-jack~#" bro
+
+if [ $bro = 1 ] || [ $bro = 1 ]
+then
+clear
+figlet "Stut-jack" | lolcat
+php tp.php
+fi
+
+if
+[ $bro = 2 ] || [ $bro = 2 ]
+then
+clear
+toilet "Stut-jack"
+php t.php
+fi
+
+if [ $bro = 3 ] || [ $bro = 3 ]
+then
+figlet "Stut-jack"
+php autolike.php
+fi
+
+if [ $bro = 4 ] || [ $bro = 4 ]
+then
+clear
+toilet "Stut-jack"
+php jk.php
+fi
+
+if [ $bro = 199 ] || [ $bro = 199 ]
+then
+clear
+toilet -f big -F gay "Stut-jack"
+sleep 1
+apt update 
+apt upgrade
+apt install python2 
+apt install git
+git clone https://github.com/avramit/Instahack
+mv Instahack $HOME
+cd $HOME/Instahack
+python2 hackinsta.py
+fi
+if [ $bro = 0 ] || [ $bro = 00 ]
+then
+echo "\033[32;1mHacker Patah Hati"
+sleep 1
+echo "\033[33;1mKarena Menjadi Baik"
+sleep 1
+echo "\033[33;1mSelalu Tersakiti"
+sleep 1
+echo "When You broke My Heart"
+sleep 1
+echo "We Will Break Your Securyti"
+sleep 1
+echo "Ketika Sebuah Hayalan Tidak tercapai"
+sleep 1
+echo "Maka Terus lah BerJuang Dan Berusaha:)"
+sleep 1
+echo "\033[32;1mKarna Suatu Hari Nanti Kamu akan Mendapatkannya:)"
+sleep 1
+exit
+fi
